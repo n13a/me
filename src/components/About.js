c7360beace73519e3ef4f68cdd64e4f61a07a902
Nima <nima97e@gmail.com>
2022-07-05 12:28:39 +0300

    finish

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function About(){
+    return (
+        <div className="about-sec">
+            <div>
+                <h4>About</h4>
+                <p>Motivated, passionate and experienced Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of Full-Stack Software. Proficient in an assortment of technologies, including JavaScript, Node.js, HTML, React, Python, CSS and MongoDB. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.</p>
+            
+            </div>
+            <div>
+                <h4>Interests</h4>
+                <p>Geek. Reader. Internet fanatic. Wannabe astronomer.</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import About from './About'
+import Footer from './Footer'
+import Info from './Info'
+
+
+export default function App(){
+    return (
+        <div className="app">
+            <Info />
+            <About />
+            <Footer />
+        </div>
+    )
+}
